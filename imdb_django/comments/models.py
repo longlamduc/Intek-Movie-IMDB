79a1b9b240a2f3643820728d67f7def9e3442071
ldlong <ldlong@intek.io>
2019-03-13 22:33:30 +0700

    finish comment field

--- imdb_django/comments/models.py
+++ imdb_django/comments/models.py
@@ -6,6 +6,3 @@ class Comment(models.Model):
    username = models.CharField(max_length=100)
    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
    content = models.CharField(max_length=500)
-
-   def __str__(self):
-       return self.title
--- imdb_django/comments/views.py
+++ imdb_django/comments/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import CommentForm
+from django.shortcuts import redirect
 from .models import Comment
 from movies.models import Movie
 
@@ -7,32 +6,18 @@ from movies.models import Movie
 def add_comment(request, movie_id=None):
     if request.method == 'POST':
         print(request.user)
-        # form = CommentForm()
-        # comment = request.POST.get('content')
-        # comment.username = str(request.user)
-        # comment.movie = Movie.objects.get(id=movie_id)
-        # comment.save()
         content = request.POST.get('content')
         movie = Movie.objects.get(id=movie_id)
         username = request.user
         comment = Comment(content=content, movie=movie, username=username)
         comment.save()
         return redirect('movie_detail', movie_id)
-    # else:
-    #     comment_form = CommentForm()
-    #     return redirect('movie_detail', movie_id)
-    # return render(request, 'comments/comment_form.html', {'form': form})
-
 
 def edit_comment(request, movie_id=None, comment_id=None):
-    item = get_object_or_404(Comment, id=comment_id)
-    form = CommentForm(request.POST or None, instance=item)
-    if form.is_valid():
-        movie = form.save(commit=False)
-        movie.save()
-        form.save_m2m()
-        return redirect('movie_detail', movie_id)
-    return render(request, 'comments/comment_form.html', {'form': form})
+    comment = Comment.objects.get(id=comment_id)
+    comment.content = request.POST.get('content')
+    comment.save()
+    return redirect('movie_detail', movie_id)
 
 def delete_comment(request, movie_id=None, comment_id=None):
     Comment.objects.get(id=comment_id).delete()
